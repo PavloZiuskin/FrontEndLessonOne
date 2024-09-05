f46cd7f7fb9a87e27d7dcad97db5b134d9024648
PavloZiuskin <passzuskin@gmail.com>
2024-09-05 20:14:22 +0300

    add-third-homework

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-let questions = prompt('What your name?');
-let secondQuestion= prompt('What your family name?');
-let nextQuestion= prompt('How old are you?');
-console.log(`${questions}, ${secondQuestion}, ${nextQuestion}`);
+let stringNumber = "10369"
+let lastNumber = stringNumber % 10;
+let fourthsNumbers =parseInt (stringNumber / 10) % 10;
+let thirdNumbers =parseInt (stringNumber / 100) % 10;
+let secondNumbers =parseInt (stringNumber / 1000) % 10;
+let firstNumbers =parseInt (stringNumber / 10000) % 10;
+
+console.log(`${firstNumbers} ${secondNumbers} ${thirdNumbers} ${fourthsNumbers} ${lastNumber}`);
+
