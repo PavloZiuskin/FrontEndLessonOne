d7fad42fac9dcecebfc2e99b0e7e3411303a89b2
PavloZiuskin <passzuskin@gmail.com>
2024-09-05 18:39:00 +0300

    add-date-type

---
+++ main.js
@@ -0,0 +1,13 @@
+const booleans= true;
+const numbers = 25;
+const strings = "Hello, my dear teacher";
+let space;
+const notANumbers = "s23";
+const nothing = null;
+
+console.log(typeof booleans)
+console.log(typeof numbers)
+console.log(typeof strings)
+console.log(typeof space)
+console.log(Math.floor(notANumbers))
+console.log(nothing)
