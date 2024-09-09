c7f68b11205e1a5914972e019016ebd0f3b930e4
PavloZiuskin <passzuskin@gmail.com>
2024-09-09 19:55:11 +0300

    add-l4hw4

--- main.js
+++ main.js
@@ -1,48 +1,17 @@
-const kiev = "Київ";
-const london = "Лондон";
-const washington = "Вашингтон";
-const football = "футбол";
-const boxe = "бокс";
-const tennis = "теніс";
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
 
-let userName = prompt("Введіть свій рік народження");
-let userCity = prompt("В якому місті ви живете?");
-let userSport = prompt("Який ваший улюблений спорт?")
-
-switch(true) {
-    case userName === null:
-        console.log("Шкода, що Ви не захотіли ввести свій(ю) рік народження");
+switch(true){
+    case numOrStr === null:
+        console.log('ви скасували')
         break;
-    case isNaN(userName):
-        console.log("Ви ввели неіснуючий вік");
+    case numOrStr.trim() === '':
+        console.log('Empty String');
         break;
-    case userName.trim() === "":
-        console.log("Шкода, що Ви не захотіли ввести свій(ю) рік народження");
+    case isNaN(+numOrStr):
+        console.log(' number is Ba_NaN');
         break;
     default:
-        let age = 2024 - userName;
-        console.log(`Ваш вік ${age}`);
+        console.log('OK!')
+        break;
 }
-
-if (userCity===null || userCity.trim()==="" || typeof(userCity) === "number" ){console.log("Шкода, що Ви не захотіли ввести свій(ю) місто");}
-    else if(userCity!== kiev&&userCity!==london&&userCity!==washington){
-        console.log(`Ви живете у місті ${userCity}`);}
-    else
-        if(userCity===washington){console.log("Ви живете у столиці Злучених Держав Америки");}
-    else
-        if (userCity===london){console.log("Ви живете у столиці Англії");}
-    else
-        if (userCity===kiev){console.log("Ви живете у столиці України");}
-        else {}
-
-
-if (userSport===null || userSport.trim()==="" || typeof(userSport) === "number" ){console.log("Шкода, що Ви не захотіли ввести свій(ю) улюблений спорт");}
-else if(userSport!== boxe&&userSport!==football&&userSport!==tennis){
-    console.log(`Ваш улюблений спорт ${userSport}`);}
-else
-if(userSport===football){console.log("Круто! Хочеш стати Мессі?");}
-else
-if (userSport===boxe){console.log("Круто! Хочеш стати Усиком?");}
-else
-if (userSport===tennis){console.log("Круто! Хочеш стати Феддерером?");}
-else {}
