87b40e460f5984cc2fd91f854f1deb0877a4ec94
PavloZiuskin <passzuskin@gmail.com>
2024-09-09 18:37:17 +0300

    add-lesson4hw1-and-branch

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-let stringNumber = "10369"
-let lastNumber = stringNumber % 10;
-let fourthsNumbers =parseInt (stringNumber / 10) % 10;
-let thirdNumbers =parseInt (stringNumber / 100) % 10;
-let secondNumbers =parseInt (stringNumber / 1000) % 10;
-let firstNumbers =parseInt (stringNumber / 10000) % 10;
-
-console.log(`${firstNumbers} ${secondNumbers} ${thirdNumbers} ${fourthsNumbers} ${lastNumber}`);
+let userName = prompt("Enter your name");
+if (userName.trim() !== "") {
+    alert(`Hello, ${userName}! How are you?`)
+}else{
+    alert("Sorry, this content is unavailable for you")
+}
 
