60c1c2080a56069a3c565209366bf2c835dc4378
PavloZiuskin <passzuskin@gmail.com>
2024-09-09 18:54:35 +0300

    add-l4hw2

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-let userName = prompt("Enter your name");
-if (userName.trim() !== "") {
-    alert(`Hello, ${userName}! How are you?`)
-}else{
-    alert("Sorry, this content is unavailable for you")
+let threeNumber = prompt("Enter number which include three numerals");
+let lastNumber = threeNumber % 10;
+let secondNumber = parseInt(threeNumber / 10) %10;
+let firstNumber = parseInt(threeNumber / 100) % 10;
+if(firstNumber === secondNumber && secondNumber===lastNumber){
+    console.log("Ви ввели три однакові цифри")
+}   else if (firstNumber === secondNumber || secondNumber === lastNumber || firstNumber === lastNumber){
+    console.log("Ви ввели дві однакові цифри")
 }
 
+if (firstNumber === secondNumber){
+    console.log("Перша та друга цифри однакові")
+}else
+if (firstNumber === lastNumber){
+    console.log("Перша та третя цифри однакові")
+}else
+if (lastNumber === secondNumber){
+    console.log("Третя та друга цифри однакові")
+}else{
+    console.log("Ви ввели три різні цифри")
+}
