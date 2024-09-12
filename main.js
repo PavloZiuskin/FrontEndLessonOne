efbd71e68f0ab7ccea477c455dba03cd7fdbbb97
PavloZiuskin <passzuskin@gmail.com>
2024-09-12 18:36:33 +0300

    add-l5hw4

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 let numbers = +prompt("Enter a number");
 
-for (let j = 1; j**2<=numbers && j>=100; j+=1){
-    console.log(j);
+for (let j = 2; j<numbers; j+=1){
+    if(numbers % j === 0){
+        console.log(`This ${numbers} is difficult`);
+        break
+    }
+    if((j+1) === numbers){console.log(numbers);}
+
+
 
 }
