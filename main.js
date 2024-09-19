93442e0a19f06befb1eee4a6fbe64834547a8291
PavloZiuskin <passzuskin@gmail.com>
2024-09-19 16:50:45 +0300

    add-l7hw2

--- main.js
+++ main.js
@@ -1,17 +1,6 @@
-const writtenString = "Hello, World";
-const removedItems= "l, d";
-
-function removeElements(someWords, deletedItems) {
-    let userWrite=someWords.split("");
-    let userDelete=deletedItems.split("");
-    userWrite.forEach((item)=>{
-        console.log(item);
-        for(let i=0;i<userDelete.length;i++){
-            if(userWrite.includes(userDelete[i]) && userDelete[i] !== " " && userDelete[i] !== ","){
-                userWrite.splice(userWrite.indexOf(userDelete[i]),1);
-            }
-        }
-    })
-    return userWrite.join(" ");
+function carryAdd(firstNumber) {
+    return function (secondNumber) {
+        return firstNumber * secondNumber;
+    }
 }
-console.log(removeElements(writtenString, removedItems));
+console.log(carryAdd(3)(4));
