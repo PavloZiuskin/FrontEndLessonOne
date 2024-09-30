5d13011c710b4415df38af97347a21fb0e409fc9
PavloZiuskin <passzuskin@gmail.com>
2024-09-30 17:20:58 +0300

    add-l9hw1

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-let ladder = {
-    step: 0,
-    up:function () {
-        this.step++;
-        return this;
-    },
-    down: function () {
-        this.step--;
-        return this;
-    },
-    showStep: function () {
-        console.log(this.step);
-        return this;
+let company = {
+    sales: [{name: "John", salary: 1000},{name: "Alice", salary: 600}],
+    shopAssistant: [{name: "Fred", salary: 1200},{name: "Maria", salary: 1000}],
+    development:{
+        web: [{name: "Peter", salary: 2000},{name: "Alex", salary: 1800}],
+        internals: [{name: "Jack", salary: 1200}]
     }
 };
-ladder.up().up().down().showStep().up().up().showStep();
+function sumSalaries(companyDepartment) {
+    if (Array.isArray(companyDepartment)) {
+        return companyDepartment.reduce((total, user) => total + user.salary, 0);
+    } else {
+        let sum = 0;
+        for (let subDepartment of Object.values(companyDepartment)) {
+            sum += sumSalaries(subDepartment);
+        }
+        return sum;
+    }
+}
+
+let totalSalaries = sumSalaries(company);
+console.log(totalSalaries);
