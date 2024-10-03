e0c96c107b548bcc109b671a241e6b1ce1d7748e
PavloZiuskin <passzuskin@gmail.com>
2024-10-03 19:56:49 +0300

    add-l10hw2

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-const userInformations = {
-    userName: 'John',
-    userAge: 22,
-    userTown: 'Kyiv',
-    userInfo: function() {
-        return `User name: ${this.userName}; User Age: ${this.userAge}; UserCity: ${this.userTown}`;
-    }
+const numbersArr= [1,2,3,4,5,22,41,169,244,333];
+
+const ewenNumbers =(numArr)=>{
+    let ewensArr=[];
+    numArr.forEach((element)=>{
+        if(element % 2 === 0){
+            ewensArr.push(element);
+        }
+    })
+    return ewensArr;
 }
 
-console.log(userInformations.userInfo());
+console.log(ewenNumbers(numbersArr));
