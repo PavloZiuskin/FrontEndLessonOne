b9e94ad2a96f34e664d623e97483daff0b6bc1c3
PavloZiuskin <passzuskin@gmail.com>
2024-10-03 19:50:26 +0300

    add-l10hw1

--- main.js
+++ main.js
@@ -1,22 +1,10 @@
-let company = {
-    sales: [{name: "John", salary: 1000},{name: "Alice", salary: 600}],
-    shopAssistant: [{name: "Fred", salary: 1200},{name: "Maria", salary: 1000}],
-    development:{
-        web: [{name: "Peter", salary: 2000},{name: "Alex", salary: 1800}],
-        internals: [{name: "Jack", salary: 1200}]
-    }
-};
-function sumSalaries(companyDepartment) {
-    if (Array.isArray(companyDepartment)) {
-        return companyDepartment.reduce((total, user) => total + user.salary, 0);
-    } else {
-        let sum = 0;
-        for (let subDepartment of Object.values(companyDepartment)) {
-            sum += sumSalaries(subDepartment);
-        }
-        return sum;
+const userInformations = {
+    userName: 'John',
+    userAge: 22,
+    userTown: 'Kyiv',
+    userInfo: function() {
+        return `User name: ${this.userName}; User Age: ${this.userAge}; UserCity: ${this.userTown}`;
     }
 }
 
-let totalSalaries = sumSalaries(company);
-console.log(totalSalaries);
+console.log(userInformations.userInfo());
