cfff27838596fe728895ddd1ea6eb41f47a6f519
PavloZiuskin <passzuskin@gmail.com>
2024-10-07 18:49:08 +0300

    add-l11hw3

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 const button = document.querySelector('.button');
-const allText = document.querySelector('#allText');
+const picture = document.querySelector('.photo');
 button.addEventListener('click', () => {
-    allText.classList.toggle('myText');
-    allText.classList.toggle('myText__changeColor');
+    let randomNum =Math.floor(Math.random() * 9) + 1;
+    let pictureActiveNumber = +picture.src.slice(picture.src.length - 5, picture.src.length -4);
+    if (randomNum === pictureActiveNumber) {
+        if (randomNum === 1)
+        {randomNum +=1;picture.src = `./image/${randomNum}.jpg`;}
+        else if (randomNum === 9)
+        {randomNum -=1;picture.src = `./image/${randomNum}.jpg`;}
+        else
+        {randomNum =Math.floor(Math.random() * 9) + 1;
+            picture.src = `./image/${randomNum}.jpg`;}
+
+    }else {console.log(22333);
+        picture.src = `./image/${randomNum}.jpg`;}
+
 })
