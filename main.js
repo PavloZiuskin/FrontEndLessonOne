748e9fffc703e6c9a265df56665b0666882c9c4f
PavloZiuskin <passzuskin@gmail.com>
2024-10-07 18:11:32 +0300

    add-l11hw2

--- main.js
+++ main.js
@@ -1,17 +1,6 @@
-
-function createTable(tableSize){
-    let myTable = document.querySelector(".table");
-    for (let i = 1; i <= tableSize; i++) {
-        let row = document.createElement("th");
-
-        for (let j = 1; j <= tableSize; j++) {
-            let cell = document.createElement("td");
-            cell.innerHTML = i * j;
-            row.appendChild(cell);
-        }
-        myTable.appendChild(row);
-
-    }
-
-};
-console.log(createTable(13));
+const button = document.querySelector('.button');
+const allText = document.querySelector('#allText');
+button.addEventListener('click', () => {
+    allText.classList.toggle('myText');
+    allText.classList.toggle('myText__changeColor');
+})
