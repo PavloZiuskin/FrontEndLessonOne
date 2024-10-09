6e3cf6483d0e632fbc47a4e67fe37f7869eae1b3
PavloZiuskin <passzuskin@gmail.com>
2024-10-09 17:45:23 +0300

    add-l12hw2

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
-const button = document.querySelector('.button');
-const picture = document.querySelector('.photo');
-button.addEventListener('click', () => {
-    let randomNum =Math.floor(Math.random() * 9) + 1;
-    let pictureActiveNumber = +picture.src.slice(picture.src.length - 5, picture.src.length -4);
-    if (randomNum === pictureActiveNumber) {
-        if (randomNum === 1)
-        {randomNum +=1;picture.src = `./image/${randomNum}.jpg`;}
-        else if (randomNum === 9)
-        {randomNum -=1;picture.src = `./image/${randomNum}.jpg`;}
-        else
-        {randomNum =Math.floor(Math.random() * 9) + 1;
-            picture.src = `./image/${randomNum}.jpg`;}
+const enteredButton = document.querySelector('.button__add_link');
+const redirectionButton = document.querySelector('.button__redirect');
+let redirectionLink = '';
 
-    }else {console.log(22333);
-        picture.src = `./image/${randomNum}.jpg`;}
-
+enteredButton.addEventListener('click', e => {
+    redirectionLink = prompt("Please enter a valid URL");
+    redirectionButton.setAttribute('href', redirectionLink);
+})
+redirectionButton.addEventListener('click', () => {
+    location.assign(redirectionLink)
 })
