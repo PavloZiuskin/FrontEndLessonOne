257e4a8012c6d65041cf64ccd2b03b60412d8e91
PavloZiuskin <passzuskin@gmail.com>
2024-11-02 17:12:49 +0200

    add-l18

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
-// Початковий час у секундах
-let startTime = 85; // Змінюйте цю змінну для встановлення часу (наприклад, 85 секунд = 1 хвилина 25 секунд)
+let startTime = 85;
 
 function startTimer(duration) {
     let timer = duration;
     const display = document.getElementById('timer');
 
     const interval = setInterval(() => {
-        // Обчислення хвилин та секунд
         const minutes = Math.floor(timer / 60);
         const seconds = timer % 60;
-
-        // Форматування у вигляді мм:сс
         display.textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-
-        // Завершення таймера
         if (timer <= 0) {
             clearInterval(interval);
             display.textContent = "00:00";
@@ -23,5 +17,4 @@ function startTimer(duration) {
     }, 1000);
 }
 
-// Запуск таймера
 startTimer(startTime);
