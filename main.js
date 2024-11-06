3e9ad74da0680ecebc7dff903e1bd6a50d51a48c
PavloZiuskin <passzuskin@gmail.com>
2024-11-06 16:15:20 +0200

    add-l19hw1

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
-let startTime = 85;
-
-function startTimer(duration) {
-    let timer = duration;
-    const display = document.getElementById('timer');
-
-    const interval = setInterval(() => {
-        const minutes = Math.floor(timer / 60);
-        const seconds = timer % 60;
-        display.textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-        if (timer <= 0) {
-            clearInterval(interval);
-            display.textContent = "00:00";
-        } else {
-            timer--;
+const API_KEY = 'e802c813c8732540c887bbaf1b88561b';
+const LATITUDE = "50.450001";
+const LONGITUDE ="30.523333";
+async function getWeather() {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${LATITUDE}&lon=${LONGITUDE}&appid=${API_KEY}`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+             new Error('Не вдалося завантажити дані');
         }
-    }, 1000);
+        const data = await response.json();
+        displayWeather(data);
+    } catch (error) {
+        document.getElementById('weather-info').innerText = 'Помилка завантаження даних';
+    }
+}
+function displayWeather(data) {
+    const temp = data.main.temp;
+    const weatherDescription = data.weather[0].description;
+    const humidity = data.main.humidity;
+    const windSpeed = data.wind.speed;
+
+    document.getElementById('weather-info').innerHTML = `
+            <p>Температура: ${temp}°C</p>
+            <p>Опис: ${weatherDescription}</p>
+            <p>Вологість: ${humidity}%</p>
+            <p>Швидкість вітру: ${windSpeed} м/с</p>
+        `;
 }
 
-startTimer(startTime);
+getWeather();
